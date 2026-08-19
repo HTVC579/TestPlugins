4060815f746b69abf4d24400a711f069301249bb
HTVC579 <vinh.htv@gmail.com>
2026-08-19 23:24:29 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -2,12 +2,21 @@ package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
 import android.content.Context
-import com.blvietsub.BLVietsubProvider
 
 @CloudstreamPlugin
 class BLVietsubPlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(BLVietsubProvider())
     }
+}
+
+class BLVietsubProvider : MainAPI() {
+    override var name = "BLVietsub"
+    override var mainUrl = "https://example.com"
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+    override var lang = "vi"
+    override val hasMainPage = true
 }
